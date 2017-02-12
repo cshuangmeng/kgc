ecb79996b90e3a460fb048a745a74715e83a64a2
huangmeng <huangmeng@kgc.com>
2017-02-12 16:15:40 +0800

    首次提交

---
+++ testProject/src/main/java/com/hm/testProject/App.java
@@ -0,0 +1,13 @@
+package com.hm.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
